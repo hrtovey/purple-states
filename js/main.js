4050c0791b31272cf7c8e3f1d34e8731d2321a2b
Heather Tovey <heather@htovey.com>
2020-11-06 16:50:27 -0700

    Remove multiple stylesheets and change colors based on class instead

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
-// https://stackoverflow.com/questions/19844545/replacing-css-file-on-the-fly-and-apply-the-new-style-to-the-page
-function changeCSS(cssFile, cssLinkIndex) {
-
-    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
-
-    var newlink = document.createElement("link");
-    newlink.setAttribute("rel", "stylesheet");
-    newlink.setAttribute("type", "text/css");
-    newlink.setAttribute("href", cssFile);
-
-    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink)
-
+function changeCSS(year) {
+    document.body.className = "";
+    document.body.classList.add(year);
 }
 
-// Add an id to the link of the year currently displayed in order to add styling
-function tagYear(el) {
 
-  var currentYear = document.getElementById("current-year");
 
-  currentYear.removeAttribute("id");
-  el.setAttribute("id", "current-year");
+function init() {
+  document.querySelectorAll('.year-selector').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
 
+      changeCSS(e.target.id);
+      document.querySelectorAll('.year-selector').forEach(function(btn) {
+        btn.classList.remove('current-year');
+      });
+      
+      btn.classList.add('current-year');
+    })
+  })
 }
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  init();
+})
